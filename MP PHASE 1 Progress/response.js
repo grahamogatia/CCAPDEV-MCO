0618cb3fc3a8dd15b99df26417a085ea8023f07b
Alyanna Cabrera <114675950+alyannacabrera@users.noreply.github.com>
2023-06-17 20:01:35 +0800

    response.js

--- MP PHASE 1 Progress/response.js
+++ MP PHASE 1 Progress/response.js
@@ -3,6 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
   var commentInput = document.getElementById("comment-input");
   var commentsList = document.getElementById("comments-list");
 
+  addComment("Thank you for rating our restaurant! We appreciate your feedback and value your opinion. We're sorry to hear that your experience with our customer service was not exceptional. We take your feedback seriously and will use it to improve our services. We hope to have another opportunity to serve you better in the future. If there's anything specific you'd like to share with us to help us improve, please feel free to let us know.", 10, 5);
+
+  addComment("We apologize for any inconvenience or disappointment you may have encountered during your visit. Your feedback is valuable to us, and we'll take it into consideration as we work on improving our customer service. We hope to have the chance to provide you with a better experience in the future. If there's anything specific you'd like to share with us to help us address your concerns, please feel free to let us know.", 25, 50);
+
+  addComment("Thank you for taking the time to rate our restaurant! We truly appreciate your feedback and are thrilled to hear that you had a fantastic experience with our customer service. Your 5-star rating means a lot to us, and we'll continue striving to provide exceptional service to all our valued customers. We look forward to serving you again in the future.", 100, 37);
+
+  addComment("We appreciate your feedback and are glad to hear that you had a positive experience with our customer service. We constantly strive to provide excellent service to all our customers, and your rating is a great encouragement for us.", 75, 5);
+
+  addComment("We're sorry to hear that you had a disappointing experience at our restaurant, and we appreciate your feedback, even if it's a 1-star rating. We apologize for failing to meet your expectations with our customer service.", 50, 123);
+
   commentForm.addEventListener("submit", function(event) {
     event.preventDefault();
   
@@ -91,7 +101,7 @@ document.addEventListener("DOMContentLoaded", function() {
     commentActions.appendChild(thumbsUpButton);
     commentActions.appendChild(thumbsDownButton);
     commentElement.appendChild(commentActions);
-
+    
     return commentElement;
   }
 
@@ -125,5 +135,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function deleteComment(commentElement) {
     commentElement.remove();
+  }
+
+  function addComment(commentText, thumbsUpCount, thumbsDownCount) {
+    var commentElement = createCommentElement(commentText);
+    var thumbsUpCountElement = commentElement.querySelector(".upvote .vote-count");
+    var thumbsDownCountElement = commentElement.querySelector(".downvote .vote-count");
+
+    updateVoteCount(thumbsUpCountElement, thumbsUpCount);
+    updateVoteCount(thumbsDownCountElement, thumbsDownCount);
+
+    commentsList.appendChild(commentElement);
   }
 });
