4c0460150fd802efce5534163b7e27a9fd67a69c
Alyanna Cabrera <114675950+alyannacabrera@users.noreply.github.com>
2023-06-23 03:25:54 +0800

    Delete MP PHASE 1 directory

--- MP PHASE 1/register.js
+++
@@ -1,6 +0,0 @@
-function showFileName() {
-    var fileName = document.getElementById("image-file");
-    var currentFileName = document.getElementById("img-file");
-    
-    currentFileName.innerHTML = fileName.files.item(0).name;
-};
